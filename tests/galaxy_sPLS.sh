33aa3b43178e9e14ab10f03af159afac234b550f
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-07-22 14:54:59 -0400

    Clean up galaxy sPLS test script

--- tests/galaxy_sPLS.sh
+++ tests/galaxy_sPLS.sh
@@ -6,4 +6,11 @@
 # Distributed under terms of the MIT license.
 #
 
-planemo test --galaxy_root ~/projects/bioinformatics/mcintyre-cid/galaxy/ --skip_venv --no_cleanup --tool_dependency_dir ~/projects/bioinformatics/mcintyre-cid/GAIT-GM/conda --test_data ~/projects/bioinformatics/mcintyre-cid/GAIT-GM/gait_gm/galaxy/test-data/ --no_conda_auto_init --no_conda_auto_install --conda_use_local tools/gait-gm/sPLS.xml
+PROJ=~/projects/bioinformatics/mcintyre-cid/GAIT-GM
+
+planemo test --galaxy_root ~/projects/bioinformatics/mcintyre-cid/galaxy/ \
+    --skip_venv --no_cleanup \
+    --tool_dependency_dir ${PROJ}/conda \
+    --test_data ${PROJ}/gait_gm/galaxy/test-data/ \
+    --no_conda_auto_init --no_conda_auto_install \
+    --conda_use_local ${PROJ}/gait_gm/galaxy/sPLS.xml
